6ba79379d6d64e8358ab26dc0add8103e68eb9f9
tanxl <tanxl@quanjing.com>
2016-09-21 16:55:39 +0800

    添加主页vo

---
+++ src/main/java/com/pdg/vo/MainVo.java
@@ -0,0 +1,169 @@
+package com.pdg.vo;
+
+import java.util.List;
+
+/***
+ * 主页显示内容
+* @Title: MainVo.java
+* @Company Beijing Panorama Media Inc.
+* @author xiaolong.Tan 
+* @date 2016年9月21日 上午11:47:36
+ */
+public class MainVo {
+	
+	/***
+	 * 学校动态
+	 */
+	private List schoolDynamic;
+	
+	/***
+	 * 行业新闻
+	 */
+	private List industryNews;
+	
+	/***
+	 * 往期学子
+	 */
+	private List formerStudent;
+	
+	/***
+	 * 学校环境 
+	 */
+	private List schoolEnvironment;
+	
+	/***
+	 * 招生问答
+	 */
+	private List FAQs;
+	
+	/***
+	 * 新闻报道
+	 */
+	private List newsReport;
+	
+	/***
+	 * 学子心声
+	 */
+	private List studentsThoughts;
+	
+	/***
+	 * 美容交流
+	 */
+	private List cosmetology;
+	
+	/***
+	 * 宠物医疗
+	 */
+	private List petCare;
+	
+	/***
+	 * 宠物饲养 
+	 */
+	private List petRaising;
+	
+	/***
+	 * 宠物新闻
+	 */
+	private List petNews;
+	
+	/***
+	 * 宠物热点
+	 */
+	private List petHotSpots;
+
+	public List getSchoolDynamic() {
+		return schoolDynamic;
+	}
+
+	public void setSchoolDynamic(List schoolDynamic) {
+		this.schoolDynamic = schoolDynamic;
+	}
+
+	public List getIndustryNews() {
+		return industryNews;
+	}
+
+	public void setIndustryNews(List industryNews) {
+		this.industryNews = industryNews;
+	}
+
+	public List getFormerStudent() {
+		return formerStudent;
+	}
+
+	public void setFormerStudent(List formerStudent) {
+		formerStudent = formerStudent;
+	}
+
+	public List getSchoolEnvironment() {
+		return schoolEnvironment;
+	}
+
+	public void setSchoolEnvironment(List schoolEnvironment) {
+		this.schoolEnvironment = schoolEnvironment;
+	}
+
+	public List getFAQs() {
+		return FAQs;
+	}
+
+	public void setFAQs(List fAQs) {
+		FAQs = fAQs;
+	}
+
+	public List getNewsReport() {
+		return newsReport;
+	}
+
+	public void setNewsReport(List newsReport) {
+		this.newsReport = newsReport;
+	}
+
+	public List getStudentsThoughts() {
+		return studentsThoughts;
+	}
+
+	public void setStudentsThoughts(List studentsThoughts) {
+		this.studentsThoughts = studentsThoughts;
+	}
+
+	public List getCosmetology() {
+		return cosmetology;
+	}
+
+	public void setCosmetology(List cosmetology) {
+		this.cosmetology = cosmetology;
+	}
+
+	public List getPetCare() {
+		return petCare;
+	}
+
+	public void setPetCare(List petCare) {
+		this.petCare = petCare;
+	}
+
+	public List getPetRaising() {
+		return petRaising;
+	}
+
+	public void setPetRaising(List petRaising) {
+		this.petRaising = petRaising;
+	}
+
+	public List getPetNews() {
+		return petNews;
+	}
+
+	public void setPetNews(List petNews) {
+		this.petNews = petNews;
+	}
+
+	public List getPetHotSpots() {
+		return petHotSpots;
+	}
+
+	public void setPetHotSpots(List petHotSpots) {
+		this.petHotSpots = petHotSpots;
+	}
+}
