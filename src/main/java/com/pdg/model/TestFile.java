1d11cbf89823958bb94f79db9000daaf1f2fdc43
tanxl <tanxl@quanjing.com>
2017-03-21 12:16:59 +0800

    sam commit

--- src/main/java/com/pdg/model/TestFile.java
+++ src/main/java/com/pdg/model/TestFile.java
@@ -1,5 +1,12 @@
 package com.pdg.model;
 
+/**
+ * 给你一坨子
+* @Title: TestFile.java
+* @Company Beijing Panorama Media Inc.
+* @author xiaolong.Tan 
+* @date 2017年3月21日 下午12:16:44
+ */
 public class TestFile {
 
 }
